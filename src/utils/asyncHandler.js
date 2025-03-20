bd037b86833b94ac9db3482d1e81df50566d2a3c
Akshit <akshit@example.com>
2025-03-20 17:19:41 +0530

    utils updata

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler=(requestHandler)=>{
+(req,res,next)=>{
+    Promise.resolve(requestHandler(req,res,next)).catch((error)=>next(error))
+}
+}
+export {asyncHandler}
+
+
+// or same code writen as 
+
+// const asynsHandler=(fn)=>async(req,res,next)=>{   // higher order function.
+
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(error.code||500).json({
+//             success:false,
+//             message:error.message
+//         })
+//     }
+// }
