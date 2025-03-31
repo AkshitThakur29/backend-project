934642302581ba99374a6f23fa3965c67b755986
Akshit <akshit@example.com>
2025-03-31 16:50:37 +0530

    routes error updates

--- src/routes/helthcheck.routes.js
+++ src/routes/helthcheck.routes.js
@@ -1,17 +1,8 @@
 import { Router } from 'express';
-import {
-    createTweet,
-    deleteTweet,
-    getUserTweets,
-    updateTweet,
-} from "../controllers/tweet.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+import { healthcheck } from "../controllers/healthcheck.controller.js"
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router.route("/").post(createTweet);
-router.route("/user/:userId").get(getUserTweets);
-router.route("/:tweetId").patch(updateTweet).delete(deleteTweet);
+router.route('/').get(healthcheck);
 
 export default router
